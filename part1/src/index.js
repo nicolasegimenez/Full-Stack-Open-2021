13e65255e98f5c5dab3e10a33b0affc12407a0f7
nicoarkano <nicoarkano@gmail.com>
2021-07-03 13:56:58 -0300

    New index.js

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-   const App =()=>(
+const App =()=>
+  {console.log('Hello from component')
+	const now = new Date()
+	const a=10
+	const b=20
+   return (
       <div>
-	 <p>Hello World</p>
-	   </div>
-   )
+	 <p>Hello World, it is {now.toString()}</p>
+	 <p>
+	    {a} plus {b} is {a + b}
 
+	   </p>	   
+      </div>
+   )
+}
 
 ReactDOM.render(<App />,  document.getElementById('root'))
 
