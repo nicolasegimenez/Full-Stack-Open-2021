c97c1b47d0ae2c77df410c5bcf4b3b4923ce51e6
nicoarkano <nicoarkano@gmail.com>
2021-07-09 17:07:55 -0300

    change console.log

--- part1/courseinfo/src/components/Total.jsx
+++ part1/courseinfo/src/components/Total.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 const Total =(props)=>{
-	 console.log(props.parts[0]['exercises'] )
 	return(<p>Number of exercises {props.parts[0]['exercises'] + props.parts[1]['exercises'] + props.parts[2]['exercises']}</p>
 	)}
 export default Total
