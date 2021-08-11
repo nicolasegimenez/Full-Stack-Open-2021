9940f908e620442256828df27080458adcc5e102
nicoarkano <nicoarkano@gmail.com>
2021-08-11 15:39:27 -0300

    Eliminate console.log

--- part1/courseinfo/src/components/Total.jsx
+++ part1/courseinfo/src/components/Total.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
 const Total =({parts})=>{
-	console.log("parts", parts)
+
 	const result = parts.map(parts => parts.exercises)
-    console.log("result",result)
+
 	
 	var total = result.reduce(function(a, b){ 
-		console.log("a ",a,"b", b)
 		return a + b; 
 	});
 
-	console.log(total)
 	return(
 
 		
