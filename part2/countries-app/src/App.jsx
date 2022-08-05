c15892eb163445031dd8209b31b7ae59e874fbcd
nicoarkano <nicoarkano@gmail.com>
2022-08-05 20:14:09 -0300

    finish the search algoritm

--- part2/countries-app/src/App.jsx
+++ part2/countries-app/src/App.jsx
@@ -1,32 +1,42 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { createPortal } from "react-dom";
 const App = () => {
-//in the country state you set the data 
+  //in the country state you set the data
   const [country, setCountry] = useState([]);
   const [findCountry, setFindCountry] = useState("");
+
   //add a countryFind to facilitate the search in useEffect
-  const countryFind = findCountry;
   const handleButton = (event) => event.preventDefault();
-  //Call a api restcountries used useEffect 
+  //1ero: Llamamos a la data con useEffect() responde un array de objetos
   useEffect(() => {
- 
     axios.get("https://restcountries.com/v3.1/all/").then((response) => {
       setCountry(response.data);
     });
   }, []);
-//Country Name is Array with 
+  //guardamos todos los paises en un array llamado "countryName"
   const countryName = country.map((countries) => countries.name.common);
- 
-  console.log("country", country.map(item => item.name.common));
 
+  //Algoritmo de busqueda multilineal busca el array[0] tipo string
+  //findCountry es una array asi que si lo pones de lleno te tira un error. Sin embargo no me gusta este codigo
 
-//This is the function you need to be use return a lowercase element
+  //Objecto Encontrado
+  const objectFind = country.find(
+    (element) => element.name.common === findCountry[0]
+  );
+  console.log("name", objectFind);
+    // Array para los lenguajes:
+
+  //Funcion que filtra los nombres de los paises con el onChange de handle find country
+
   const handleFindCountry = (event) => {
     const finder = countryName.filter((namesFind) =>
       namesFind.toLowerCase().includes(event.target.value.toLowerCase())
     );
     setFindCountry(finder);
   };
+
+
   return (
     <div>
       <h1>Countries App </h1>
@@ -37,7 +47,19 @@ const App = () => {
       <div>
         <ul>
           {Array.isArray(findCountry)
-            ? findCountry.map((findCountries) => (<li>{findCountries}</li>))
+            ? findCountry.map((findCountries) => (
+                <>
+                  <h2>{objectFind.name.common}</h2>
+                  <li>Capital: {objectFind.capital} </li>
+                  <li>Population: {objectFind.population}</li>
+                  <h2>Languages</h2>
+                 {Object.keys(objectFind.languages).map(function(key){ 
+                    return ( <li>{objectFind.languages[key]}</li>
+                    ) 
+                  } )}
+                  <img src={objectFind.flags.png}></img>
+                </>
+              ))
             : []}
         </ul>
       </div>
